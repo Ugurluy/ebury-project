7604644d933daae1dfefd4c1e55ae896c26dc7e5
Yagiz Ugurlu <yagiz.ugurlu02@bilgiedu.net>
2022-05-16 10:25:56 +0300

    API Key change

--- src/services/HttpService.ts
+++ src/services/HttpService.ts
@@ -7,7 +7,7 @@ const service = axios.create();
  * @description axios interceptor for replacing baseURL a& adding api key
  */
 service.interceptors.request.use((config: any) => {
-  config.headers["apikey"] = "4LKv7p4JSqBUWA7mt3cGAIDCUysr7fdk";
+  config.headers["apikey"] = "bLWFrLDPKFbfWVWB1NlkTtPSs56YZJjy";
   config.url = config.url.replace("[api]", env.API_SERVICE);
   return config;
 });
